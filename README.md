76bf3480fd2c78be8e98fac19ce4f9ef4d84c03d
Alejandro Morales <vamg008@gmail.com>
2012-07-10 20:01:41 -0600

    [dist] Open source all the things. gridspot-v0.0.0

---
+++ README.md
@@ -0,0 +1,45 @@
+# gridspot
+
+Gridspot for the rest of us.
+
+**node.js cli-tool**
+
+## Getting Started
+
+Install the module with: `npm install gridspot -g`. Then, just do `gridspot`.
+
+
+## Use
+
+	Gridspot for your cli
+
+	usage:
+
+		gridspot <API_KEY>               Simple usage. <API_KEY> from https://gridspot.com/compute/account
+		gridspot -s,--save <API_KEY>     Save the <API_KEY> to ~/.gridspot so future request you can avoid the api key.
+		gridspot -i <API_KEY>            Interactive response (if available)
+		gridspot -r, --raw <API_KEY>     Raw response
+		gridspot -v, --version           Current version
+		gridspot -h, --help              Show this help
+
+    By Default it'll only show the ssh servers
+
+
+
+## Examples
+
+By default `gridspot` will show you the ip of your servers and the status (`gridspot <api_key>`)
+
+	$ gridspot -s apikey_A2iG6Uvrug2B-qsmWuwX5g # to save the api to the fs for future requests
+	$ gridspot -i # interactive mode (using cdir)
+	$ gridspot --raw # raw response
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [grunt](https://github.com/cowboy/grunt). And idiomatic.js.
+
+## Release History
+- `0.0.0` - July
+
+## License
+Copyright (c) 2012 Alejandro Morales  
+Licensed under the MIT license.
---
+++ index.js
@@ -0,0 +1,98 @@
+/*
+ * gridspot
+ * https://github.com/alejandromg/gridspot
+ *
+ * Copyright (c) 2012 Alejandro Morales
+ * Licensed under the MIT license.
+ */
+/*jshint laxcomma:true, debug:true, node:true */
+
+"use strict";
+
+var request = require('request')
+  , fs      = require('fs')
+  , path    = require('path')
+  , params = {}
+  , cfg    = {}
+  , config = {};
+
+cfg.file = path.join(process.env.HOME, '.gridspot');
+
+var API_KEY = process.argv[2] || process.argv[3]; // just if --k <key>
+
+try {
+    config = JSON.parse(fs.readFileSync(cfg.file, 'utf8'));
+    console.dir = require('cdir');
+} catch (exc) {
+    if (!API_KEY || !config.api) showHelp();
+}
+
+
+var reg = new RegExp('=.*', 'gi');
+
+for (var key in process.argv){
+    var attr = process.argv[key];
+
+    if (/--/gi.test(attr) || /-/gi.test(attr)){
+        var attri = attr.replace(/--/,'')
+                        .replace(/-/,'')
+                        .replace(reg,'');
+        if (reg.test(attr)){
+            params[attri] = attr.split('=')[1];
+        } else {
+            params[attri] = process.argv[++key] || true;
+        }
+    }
+}
+
+function showHelp (){
+    console.log('\n\tGridspot for your cli\n');
+    console.log('\tusage:\n');
+    console.log('\t\tgridspot <API_KEY>\t\t Simple usage. <API_KEY> from https://gridspot.com/compute/account');
+    console.log('\t\tgridspot -s,--save <API_KEY>\t Save the <API_KEY> to ~/.gridspot so future request you can avoid the api key.');
+    console.log('\t\tgridspot -i <API_KEY>\t\t Interactive response (if available)');
+    console.log('\t\tgridspot -r, --raw <API_KEY>\t Raw response');
+    console.log('\t\tgridspot -v, --version \t\t Current version');
+    console.log('\t\tgridspot -h, --help \t\t Show this help');
+    console.log('\n\tBy Default it\'ll only show the ssh servers');
+    console.log('\n');
+    process.exit(1);
+}
+
+if (params.help || params.h) return showHelp();
+
+if (params.s || params.save) {
+    params.s = params.s || params.save;
+    fs.writeFile(cfg.file, JSON.stringify({api: params.s}, null, 2), function (error, response){
+        if (error) throw error;
+        console.log('Your api key is saved now.');
+        process.exit(1);
+    });
+}
+
+if (params.version || params.v) {
+    console.log(require('./package.json').version);
+    process.exit(1);
+}
+
+var key = API_KEY || config.api;
+var loop = 0;
+
+if (key) {
+    console.log('Requesting your instances');
+    request({
+        uri: 'https://gridspot.com/compute_api/v1/list_instances',
+        port: 443,
+        qs: {'api_key': key}
+    }, function (error, response){
+        if (error) throw error;
+        if (~response.body.search('<html')) throw new Error('The response is wrong malformed');
+        response.body = JSON.parse(response.body);
+        if (params.raw) return console.log(JSON.stringify(response.body, null, 2));
+        if (params.i) return console.dir(response.body);
+        console.log('Servers: (id, ip, state)');
+        response.body.instances.forEach(function (instance) {
+            console.log('\t',++loop+'.- ', instance.vm_ssh_wan_ip_endpoint, instance.current_state);
+        });
+    });
+}
